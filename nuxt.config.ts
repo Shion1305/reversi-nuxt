bf757c1e44b58115162341cc916d7fd95a8fae73
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 02:49:18 +0900

    🔧 NuxtConfig runtimeConfigにFirebase設定フィールドを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    runtimeConfig: {
-        firebase: {
-            apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            appId: process.env.FIREBASE_APP_ID
-        }
+  ssr: false,
+  runtimeConfig: {
+    public: {
+      firebase: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        appId: process.env.FIREBASE_APP_ID
+      }
     }
+  }
 })
