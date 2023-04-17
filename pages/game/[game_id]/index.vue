21c6f8800df0849ebb3753e486c0244cd4e759e8
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 05:55:29 +0900

    ✨ ReversiBoardの改良, Firebaseとの連携

--- pages/game/[game_id]/index.vue
+++ pages/game/[game_id]/index.vue
@@ -1,20 +1,34 @@
 <script setup>
-import ReversiBoard from "~/components/ReversiBoard.vue";
+import ReversiBoard from '~/components/ReversiBoard.vue'
+import { getFirestore, doc, onSnapshot, setDoc } from 'firebase/firestore'
 
-const boardData = ref([
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 2, 0, 0, 0],
-  [0, 0, 0, 2, 1, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0]
-])
+const boardData = ref([])
+
+//get game_id from url
+const route = useRoute()
+const gameID = route.params.game_id
+
+// listen to GameData from firestore
+console.log('gameID', gameID)
+const db = getFirestore()
+const gameRef = doc(db, 'games', gameID)
+onSnapshot(gameRef, (doc) => {
+  boardData.value = doc.data().boardData
+})
+// setDoc(gameRef, {
+//   boardData: [
+//     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+//   ]
+// })
+const logClick = function (row, col) {
+  console.log(row, col)
+}
 </script>
 
 <template>
-  <ReversiBoard :boardData="boardData" />
+  <ReversiBoard :boardData="boardData" @cell-click="logClick" />
 </template>
 
 <style lang="scss" scoped>
