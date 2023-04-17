a22d47d111fb609ad1f630ff6ec5636108e1ac93
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 06:24:33 +0900

    🐛 Specify SSR:false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   plugins: ['~/plugins/firebase.client.ts'],
+  ssr: false,
   runtimeConfig: {
     public: {
       firebase: {
