35a34e60ddfcc2c0399d0e1996484d92da0e19f0
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 05:54:30 +0900

    🎨 (plugins/firebase)useRuntimeConfigの適用,firebaseのための整備

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  plugins: ['~/plugins/firebase.client.ts'],
   runtimeConfig: {
     public: {
       firebase: {
--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,33 +1,17 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import { getAnalytics } from 'firebase/analytics'
-
-// tried to replace with useRuntimeConfig().public but was unable to load useRuntimeConfig
-const firebaseConfig = {
-  apiKey: 'AIzaSyAXS55p_9dlpwEAGDSgxIMZAEWQQkwrEwE',
-  authDomain: 'ynuprojectlearning2023-04.firebaseapp.com',
-  projectId: 'ynuprojectlearning2023-04',
-  storageBucket: 'ynuprojectlearning2023-04.appspot.com',
-  messagingSenderId: '808452933891',
-  appId: '1:808452933891:web:9e3763cfcc5d1a5c318136'
-}
-
-// const firebaseConfig = {
-//   apiKey: config.firebase.apiKey,
-//   authDomain: config.firebase.authDomain,
-//   projectId: config.firebase.projectId,
-//   appId: config.firebase.appId
-// }
+import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig().public
+  const firebaseConfig = {
+    apiKey: config.firebase.apiKey,
+    authDomain: config.firebase.authDomain,
+    projectId: config.firebase.projectId,
+    appId: config.firebase.appId
+  }
   const app = initializeApp(firebaseConfig)
-
-  const analytics = getAnalytics(app)
-  const auth = getAuth(app)
-  const firestore = getFirestore(app)
-
-  nuxtApp.provide('firebaseAnalytics', analytics)
-  nuxtApp.provide('firebaseAuth', auth)
-  nuxtApp.provide('firebaseFirestore', firestore)
+  const db = getFirestore(app)
+  // Firestoreに接続するための情報をアプリケーション全体に提供
+  nuxtApp.provide('db', db)
 })
