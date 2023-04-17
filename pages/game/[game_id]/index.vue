01c6ffb21469c97d7bea4702c59fd2059687aee0
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 01:48:55 +0900

    ✨ ReversiBoardの適用

--- pages/game/[game_id]/index.vue
+++ pages/game/[game_id]/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+import ReversiBoard from "~/components/ReversiBoard.vue";
+
 const boardData = ref([
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -12,13 +14,7 @@ const boardData = ref([
 </script>
 
 <template>
-  <div class="board">
-    <div v-for="row in boardData" class="board-row">
-      <div v-for="s in row" class="board-cell">
-        <div class="cell" :class="s === 0 ? 'empty' : s === 1 ? 'black' : 'white'"></div>
-      </div>
-    </div>
-  </div>
+  <ReversiBoard :boardData="boardData" />
 </template>
 
 <style lang="scss" scoped>
@@ -29,46 +25,5 @@ body {
   height: 100vh;
   margin: 0;
   background-color: #f0f0f0;
-}
-
-.board {
-  display: grid;
-  grid-template-columns: repeat(8, 1fr);
-  grid-template-rows: repeat(8, 1fr);
-  width: 400px;
-  height: 400px;
-  border: 2px solid #000;
-  background-color: #228b22;
-}
-
-.board-row {
-  display: contents;
-}
-
-.board-cell {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  border: 1px solid #000;
-}
-
-.cell {
-  width: 80%;
-  height: 80%;
-  border-radius: 50%;
-
-  &.empty {
-    background-color: transparent;
-  }
-
-  &.black {
-    background-color: #000;
-  }
-
-  &.white {
-    background-color: #fff;
-  }
 }
 </style>
