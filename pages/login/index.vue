733b7e5b84e3d3ddd16e1e7309adef741aa4df7a
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 07:45:25 +0900

    ✨ LineLoginページ

---
+++ pages/login/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+const config = useRuntimeConfig()
+
+const loginURL =
+  'https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=' +
+  config.line.clientID +
+  '&redirect_uri=' +
+  config.line.callbackURI +
+  '&state=' +
+  'test111' +
+  '&scope=openid%20profile%20email'
+</script>
+<template>
+  <a :href="loginURL">ログインする</a>
+</template>
+
+<style scoped></style>
