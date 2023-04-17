126d9dec60abf7b2ba3eca9ee95bf29053d3d946
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 03:25:13 +0900

    🐛 (plugin)firebase.client.tsのruntimeConfig読み込みを解除

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -3,14 +3,22 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getAnalytics } from 'firebase/analytics'
 
-const config = useRuntimeConfig().public
-
+// tried to replace with useRuntimeConfig().public but was unable to load useRuntimeConfig
 const firebaseConfig = {
-  apiKey: config.firebase.apiKey,
-  authDomain: config.firebase.authDomain,
-  projectId: config.firebase.projectId,
-  appId: config.firebase.appId
+  apiKey: 'AIzaSyAXS55p_9dlpwEAGDSgxIMZAEWQQkwrEwE',
+  authDomain: 'ynuprojectlearning2023-04.firebaseapp.com',
+  projectId: 'ynuprojectlearning2023-04',
+  storageBucket: 'ynuprojectlearning2023-04.appspot.com',
+  messagingSenderId: '808452933891',
+  appId: '1:808452933891:web:9e3763cfcc5d1a5c318136'
 }
+
+// const firebaseConfig = {
+//   apiKey: config.firebase.apiKey,
+//   authDomain: config.firebase.authDomain,
+//   projectId: config.firebase.projectId,
+//   appId: config.firebase.appId
+// }
 
 export default defineNuxtPlugin((nuxtApp) => {
   const app = initializeApp(firebaseConfig)
@@ -18,4 +26,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   const analytics = getAnalytics(app)
   const auth = getAuth(app)
   const firestore = getFirestore(app)
+
+  nuxtApp.provide('firebaseAnalytics', analytics)
+  nuxtApp.provide('firebaseAuth', auth)
+  nuxtApp.provide('firebaseFirestore', firestore)
 })
