b6c5cb708ec602bf728b9de3ff820ae99b5ca289
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 05:53:08 +0900

    🎨 ReversiBoardの改良,Props/Emitの改良

--- components/ReversiBoard.vue
+++ components/ReversiBoard.vue
@@ -1,69 +1,77 @@
 <script lang="ts" setup>
+const props = withDefaults(defineProps<{ boardData: any }>(), {
+  boardData: [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ]
+})
+const boardRows = computed(() => {
+  const size = 8
+  const result = []
+  for (let i = 0; i < props.boardData.length; i += size) {
+    result.push(props.boardData.slice(i, i + size))
+  }
+  return result
+})
 
-const props = withDefaults(defineProps<{ boardData: number[][] }>(),
-  {
-    boardData: () => [
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 1, 2, 0, 0, 0],
-      [0, 0, 0, 2, 1, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-  })
+const emits =
+  defineEmits<(e: 'cell-click', index1: number, index2: number) => void>()
 </script>
 
 <template>
   <div class="board">
-    <div v-for="row in props.boardData" class="board-row">
-      <div v-for="s in row" class="board-cell">
-        <div class="cell" :class="s === 0 ? 'empty' : s === 1 ? 'black' : 'white'"></div>
+    <div v-for="(row, index1) in boardRows" :key="row.index" class="board-row">
+      <div v-for="(s, index2) in row" class="board-cell">
+        <div
+          class="cell"
+          :class="s === 0 ? 'empty' : s === 1 ? 'black' : 'white'"
+          @click="emits('cell-click', index1, index2)"
+        ></div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .board {
-    display: grid;
-    grid-template-columns: repeat(8, 1fr);
-    grid-template-rows: repeat(8, 1fr);
-    width: 400px;
-    height: 400px;
-    border: 2px solid #000;
-    background-color: #228b22;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: repeat(8, 1fr);
+  width: 400px;
+  height: 400px;
+  border: 2px solid #000;
+  background-color: #228b22;
 }
 
 .board-row {
-    display: contents;
+  display: contents;
 }
 
 .board-cell {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    border: 1px solid #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #000;
 }
 
 .cell {
-    width: 80%;
-    height: 80%;
-    border-radius: 50%;
+  width: 80%;
+  height: 80%;
+  border-radius: 50%;
 
-    &.empty {
-        background-color: transparent;
-    }
+  &.empty {
+    background-color: transparent;
+  }
 
-    &.black {
-        background-color: #000;
-    }
+  &.black {
+    background-color: #000;
+  }
 
-    &.white {
-        background-color: #fff;
-    }
+  &.white {
+    background-color: #fff;
+  }
 }
 </style>
