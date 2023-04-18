9ff8e5bdb63e1f84d3218515cc0b441fab44b082
Shion Ichikawa <shion1305@gmail.com>
2023-04-18 11:44:06 +0900

    ✨ jwt for server directory

---
+++ server/jwt.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import { JWTPayload } from 'jose'
+
+const config = useRuntimeConfig()
+
+const SECRET_KEY = config.jwt.secret
+
+const generateToken = (id: string) => {
+  return jwt.sign(id, SECRET_KEY, { expiresIn: '7d', algorithm: 'HS256' })
+}
+
+const verifyToken = (token: string): string | null => {
+  try {
+    const id = jwt.verify(token, SECRET_KEY, { algorithms: ['HS256'] })
+    console.log('decoded id: ', id)
+    return id as string
+  } catch (error) {
+    return null
+  }
+}
+
+export { generateToken, verifyToken }
