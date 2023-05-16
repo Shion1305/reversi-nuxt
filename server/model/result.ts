db78089478e3fa3b2293bbf0856420bf3d6658b0
ab84ihs <ab84ihs@gmail.com>
2023-05-16 15:58:51 +0900

    サーバー用のデータ構造の宣言

---
+++ server/model/result.ts
@@ -0,0 +1,10 @@
+interface Result{
+    id : string
+    users : string[]
+    black_user : string
+    white_user : string
+    black_num : Number
+    white_num : Number
+    winner : "black"|"white"|"draw"
+    time : Date
+}
