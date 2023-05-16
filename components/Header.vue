51de580244dfae80428b966614e7991e4b630f2b
Shion Ichikawa <shion1305@gmail.com>
2023-05-16 13:25:41 +0900

    ✨ default layoutの仮完成

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header>
+    <img
+      alt="ひっくりかえる ヘッダー画像"
+      src="@/assets/imgs/header-logo.png"
+    />
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  width: 100%;
+  height: 120px;
+  display: flex;
+
+  img {
+    box-sizing: border-box;
+    margin: auto;
+    padding: 10px;
+    height: 100%;
+    max-width: 100vw;
+    object-fit: contain;
+  }
+}
+</style>
