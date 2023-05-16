412132b87029b60126779b5060836dedb7c503bb
(Takuto Tomita) <kuroko.no.18@gmail.com>
2023-05-16 15:47:23 +0900

    Readmeを変更した

--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -3,17 +3,40 @@
 </script>
 
 <template>
-<div class="flex">
-    <img src="@/assets/imgs/frog_with_board_green.png" alt="">
+<div class="relative">
+    <img src="@/assets/imgs/frog_with_board_green.png" alt="" class="frog">
+    <h1 class="absolute01">名前は？</h1>
     <img src="@/assets/imgs/otamajakusi.png" alt="">
-    <h1>名前は？</h1>
+    <input class="absolute02" type="text">
+
 </div>
 </template>
 
 <style scoped>
-.flex{
-    display:flex;
-    flex-direction:column;
-
+.relative{
+    display: flex;
+    position:relative;
+    flex-direction: column;
+    align-items: center;
 }
+.frog{
+    width:300px;
+    height:300px;
+}
+.absolute01{
+    position: absolute;
+    text-align: center;
+    font-size: 50px;
+    top:110px;
+}
+
+.absolute02{
+    position:absolute;
+    font-size: 300%;
+    top:400px;
+    right:700px;
+    width:350px;
+    height:100px;
+}
+
 </style>
