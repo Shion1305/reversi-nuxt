1712c9aa0e2a352ccdc13ef6c080ea9dad49a62c
(Takuto Tomita) <kuroko.no.18@gmail.com>
2023-05-16 15:07:33 +0900

    Readmeを変更した

---
+++ pages/setting/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex">
+    <img src="@/assets/imgs/frog_with_board_green.png" alt="">
+    <img src="@/assets/imgs/otamajakusi.png" alt="">
+    <h1>名前は？</h1>
+</div>
+</template>
+
+<style scoped>
+.flex{
+    display:flex;
+    flex-direction:column;
+
+}
+</style>
