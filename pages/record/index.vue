0bf1de39fe2be2f72875c9d13b232186bf07c73b
2164178 <nakagawa-haruhito-nd@ynu.jp>
2023-05-18 23:42:46 +0900

    git から .gitignoreを除外

--- pages/record/index.vue
+++ pages/record/index.vue
@@ -16,7 +16,7 @@
         </div>
 
         <div class="table">
-            <table border="1" bgcolor="#90ee90" width="750" height="150">
+            <table border="1" bgcolor="#90ee90" width="500" height="150">
 
                 <tr height="20">
                     <th>名前</th>
@@ -51,6 +51,53 @@
                 </tr>
             </table>
         </div>
+        <div class="table2">
+            <table border="1" bgcolor="#90ee90" width="625" height="150">
+
+                <tr height="20">
+                    <th>名前</th>
+                    <th>勝ち</th>
+                    <th>負け</th>
+                    <th>引分</th>
+                    <th>投了</th>
+                </tr>
+
+                <tr height="40">
+                    <td>太郎</td>
+                    <td>３２</td>
+                    <td>　０</td>
+                    <td>　０</td>
+                    <td>　０</td>
+
+                </tr>
+                <tr height="40">
+                    <td>次郎</td>
+                    <td>３０</td>
+                    <td>　２</td>
+                    <td>　０</td>
+                    <td>　０</td>
+                </tr>
+                <tr height="40">
+                    <td>三郎</td>
+                    <td>１０</td>
+                    <td>２２</td>
+                    <td>　０</td>
+                    <td>　０</td>
+                </tr>
+                <tr height="40">
+                    <td>太郎</td>
+                    <td>勝ち</td>
+                    <td>６０</td>
+                    <td>　４</td>
+                </tr>
+            </table>
+        </div>
+        <div class="m1">
+        <p><font size="5">直近の戦績</font></p>
+        </div>
+        <div class="m2">
+            <p><font size="5">相手毎の通算戦績</font></p>
+        </div>
     </div>
 </template>
 
@@ -72,10 +119,18 @@
 }
 .table{
     position: absolute;
-    left: 250px;
+
     bottom: 50px;
     overflow-y: scroll;
-    width: 767px;
+    width: 517px;
+    height: 150px;
+}
+.table2{
+    position: absolute;
+    left: 580px;
+    bottom: 50px;
+    overflow-y: scroll;
+    width: 642px;
     height: 150px;
 }
 .table th{
@@ -83,6 +138,21 @@
     position: sticky;
     top: 0;
     left: 0;
+}
+.table2 th{
+    background: forestgreen;
+    position: sticky;
+    top: 0;
+    left: 0;
+}
+.m1{
+    position: absolute;
+    bottom: 180px;
+}
+.m2{
+    position: absolute;
+    right: 0;
+    bottom: 180px;
 }
 
 </style>
