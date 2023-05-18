915181f14e36d9597a98f48b2b8dbc4f563767af
Shion Ichikawa <shion1305@gmail.com>
2023-05-19 07:54:39 +0900

    (model)Gameにusersの追加

--- server/model/game.ts
+++ server/model/game.ts
@@ -1,13 +1,15 @@
-import {Disc} from '~/server/model/disc'
+import { Disc } from '~/server/model/disc'
+
 export interface Game {
-    id : string
-    board : Disc[]
-    turn : "black"|"white"
-    black_user : string
-    white_user : string
-    black_num : Number
-    white_num : Number
-    black_time : Date
-    white_time : Date
-    end : boolean
+  id: string
+  board: Disc[]
+  turn: 'black' | 'white'
+  users: string[]
+  black_user: string
+  white_user: string
+  black_num: Number
+  white_num: Number
+  black_time: Date
+  white_time: Date
+  end: boolean
 }
