3cebc5e890fcfec9b00050922db163b4c8cfd0fc
Shion Ichikawa <shion1305@gmail.com>
2023-05-21 10:08:26 +0900

    ✨ (server/model)updatePossibleDiscs

--- server/model/reversiBoard.ts
+++ server/model/reversiBoard.ts
@@ -56,9 +56,75 @@ export class ReversiBoard {
     return validMove ? newBoard : null
   }
 
-  public exportBoard(): Disc[] {
-    return [...this.board]
+  private updatePossibleDiscs(): void {
+    for (let i = 0; i < 64; i++) {
+      if (this.game.board[i] === Disc.EMPTY_POSSIBLE) {
+        this.game.board[i] = Disc.EMPTY
+      }
+    }
+
+    const directions: number[][] = [
+      [1, 0],
+      [0, 1],
+      [-1, 0],
+      [0, -1],
+      [1, 1],
+      [-1, 1],
+      [-1, -1],
+      [1, -1]
+    ]
+    const opponentDisc = DiscRole.getDisc(
+      DiscRole.getOpponentDisc(this.game.turn)
+    )
+
+    for (let i = 0; i < 64; i++) {
+      if (this.game.board[i] !== Disc.EMPTY) {
+        continue
+      }
+
+      const originX = i % 8
+      const originY = Math.floor(i / 8)
+
+      for (const direction of directions) {
+        let currentX = originX + direction[0]
+        let currentY = originY + direction[1]
+        let flipped = false
+
+        while (
+          this.game.board[currentX + currentY * 8] === opponentDisc &&
+          currentX >= 0 &&
+          currentX < 8 &&
+          currentY >= 0 &&
+          currentY < 8
+        ) {
+          currentX += direction[0]
+          currentY += direction[1]
+          flipped = true
+        }
+
+        if (
+          flipped &&
+          this.game.board[currentX + currentY * 8] ===
+            DiscRole.getDisc(this.game.turn)
+        ) {
+          this.game.board[i] = Disc.EMPTY_POSSIBLE
+        }
+      }
+    }
   }
 
-  // private getDis
+  public countDiscs(): { black: number; white: number } {
+    let black = 0
+    let white = 0
+
+    for (const disc of this.game.board) {
+      if (disc === Disc.BLACK) {
+        black++
+      } else if (disc === Disc.WHITE) {
+        white++
+      }
+    }
+
+    return { black, white }
+  }
 }
