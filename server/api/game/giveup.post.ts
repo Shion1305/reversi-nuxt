6e83cb0f32098456c2a9e8b63de8322bd162170f
Shion Ichikawa <shion1305@gmail.com>
2023-05-22 20:24:41 +0900

    🚧 GiveUp用のapi

---
+++ server/api/game/giveup.post.ts
@@ -0,0 +1,52 @@
+import { PassRequest } from '~/server/api/game/pass.post'
+import { getGameByID } from '~/server/pkg/game'
+
+export default defineEventHandler(async (event) => {
+  const userID = event.context.userID
+  if (!userID) {
+    return createError({
+      statusCode: 401,
+      statusMessage: 'Unauthorized'
+    })
+  }
+
+  const req = await readBody(event)
+    .then((body) => {
+      return body as PassRequest
+    })
+    .catch(() => {
+      return null
+    })
+  if (!req || !req.gameID) {
+    return createError({
+      statusCode: 400,
+      statusMessage: 'Bad Request'
+    })
+  }
+
+  const game = await getGameByID(req.gameID)
+  if (!game) {
+    return createError({
+      statusCode: 404,
+      statusMessage: 'Not Found'
+    })
+  }
+
+  if (
+    game.users[0] !== userID &&
+    game.users.length === 2 &&
+    game.users[1] !== userID
+  ) {
+    return createError({
+      statusCode: 403,
+      statusMessage: 'Forbidden'
+    })
+  }
+
+  if (game.end) {
+    return createError({
+      statusCode: 404,
+      statusMessage: 'No Active Game Found, Game is over'
+    })
+  }
+})
