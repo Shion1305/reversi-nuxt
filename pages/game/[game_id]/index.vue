9689b4750a7edbd70b1eab7914e684b72c69347f
Shion Ichikawa <shion1305@gmail.com>
2023-05-22 19:20:59 +0900

    ✨ 対局画面のAPI連携

--- pages/game/[game_id]/index.vue
+++ pages/game/[game_id]/index.vue
@@ -1,43 +1,63 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import ReversiBoard from '~/components/ReversiBoard.vue'
 import {
-  getFirestore,
   doc,
-  onSnapshot,
-  DocumentReference
+  DocumentReference,
+  getFirestore,
+  onSnapshot
 } from 'firebase/firestore'
-import { GameData } from '~/types/game'
+import { Game } from '~/model/game'
+import axios from 'axios'
 
-const data: { gameData: GameData } = reactive({
+const data: { gameData: Game } = reactive({
   gameData: {
     board: []
-  } as GameData
+  } as Game
 })
 
 //get game_id from url
 const route = useRoute()
 const gameID: string = route.params.game_id as string
 
-// listen to GameData from firestore
+// listen to Game from firestore
 console.log('gameID', gameID)
 const db = getFirestore()
-const gameRef = doc(db, 'games', gameID) as DocumentReference<GameData>
+const gameRef = doc(db, 'games', gameID) as DocumentReference<Game>
 onSnapshot(gameRef, (doc) => {
   if (!doc.exists()) useRouter().push('/')
-  data.gameData = doc.data() as GameData
-  console.log('doc', data.gameData.board)
+  data.gameData = doc.data() as Game
+  data.gameData.id = doc.id
 })
 const logClick = function (row, col) {
   console.log(row, col)
+}
+const onPlace = function (row, col) {
+  axios.post('/api/game/place', {
+    gameID: gameID,
+    index: row * 8 + col
+  })
+}
+
+const passAction = function () {
+  axios.post('/api/game/pass', {
+    gameID: gameID
+  })
 }
 </script>
 
 <template>
   <div class="page-frame">
-    <div id="result-pane"></div>
+    <div id="result-pane">
+      <div id="opponent-panel">aaaa</div>
+      <ScoreBoard
+        :black="data.gameData.black_num"
+        :white="data.gameData.white_num"
+      />
+      <button @click="passAction">PASS</button>
+    </div>
     <div id="board-pane">
       <div id="board-container">
-        <ReversiBoard :board="data.gameData.board" @cell-click="logClick" />
+        <ReversiBoard :board="data.gameData.board" @cell-click="onPlace" />
       </div>
     </div>
   </div>
@@ -65,6 +85,12 @@ const logClick = function (row, col) {
   padding: 5%;
   box-sizing: border-box;
   margin: auto;
+  background-image: url('@/assets/imgs/big_lotus.png');
+  background-size: contain;
+
+  > div {
+    width: 70%;
+  }
 }
 
 #board-pane {
@@ -72,6 +98,9 @@ const logClick = function (row, col) {
 }
 
 #result-pane {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   right: 0;
   width: 400px;
   height: 100%;
