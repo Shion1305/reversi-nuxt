2c10737281b0a001866eb29600cfb79dd91e2576
Shion Ichikawa <shion1305@gmail.com>
2023-05-22 14:19:53 +0900

    🐛 (server/middleware)authMiddlewareの修正

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,18 +1,12 @@
 import { verifyToken } from '~/server/jwt'
 
 export default defineEventHandler((event) => {
-  const authHeader = event.req.headers.authorization
+  const token = getCookie(event, 'authToken')
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1]
-
-    if (token) {
-      const auth = verifyToken(token)
-      if (auth) {
-        event.context.userID = auth.id
-      }
-    } else {
-      console.log('No Bearer Token found')
+  if (token) {
+    const auth = verifyToken(token)
+    if (auth) {
+      event.context.userID = auth.id
     }
   } else {
     console.log('No Authorization header')
