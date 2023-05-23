9adee04593ebbef8fe876a9ef4284f8cff0ae68b
Shion Ichikawa <shion1305@gmail.com>
2023-05-23 14:48:58 +0900

    🐛 名前変更画面の修正

--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -21,52 +21,69 @@ const changeName = async () => {
 </script>
 
 <template>
-  <div class="relative">
-    <img src="@/assets/imgs/frog_with_board_green.png" alt="" class="frog" />
-    <h1 class="absolute01">名前は？</h1>
-    <img src="@/assets/imgs/otamajakusi.png" alt="" />
+  <div class="base-frame">
+    <div id="board-frame">
+      <img src="@/assets/imgs/frog_with_board_green.png" alt="" class="frog" />
+      <h1 class="absolute01">名前は？</h1>
+    </div>
     <input class="absolute02" type="text" v-model="nameRef" />
     <input class="absolute03" type="submit" value="OK" @click="changeName" />
+    <NuxtLink class="link" to="/">戻る</NuxtLink>
   </div>
-  <NuxtLink to="/">戻る</NuxtLink>
 </template>
 
 <style scoped>
-.relative {
+.base-frame {
   display: flex;
-  position: relative;
   flex-direction: column;
   align-items: center;
+
+  .link {
+    margin: 30px;
+    padding: 5px;
+    border-radius: 10px;
+    text-decoration: none;
+    color: black;
+    font-size: 50px;
+    background: white;
+    border: 2px solid black;
+  }
 }
 
-.frog {
-  width: 300px;
-  height: 300px;
+#board-frame {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .frog {
+    width: 300px;
+    height: 300px;
+  }
+  .absolute01 {
+    position: absolute;
+  }
 }
 
-.absolute01 {
-  position: absolute;
-  text-align: center;
-  font-size: 50px;
-  top: 110px;
-}
+/*.absolute01 {*/
+/*  position: absolute;*/
+/*  top: 0;*/
+/*  left: 0;*/
+/*  font-size: 50px;*/
+/*  width: 350px;*/
+/*  height: 100px;*/
+/*}*/
 
 .absolute02 {
-  position: absolute;
-  font-size: 300%;
-  top: 400px;
-  right: 700px;
+  font-size: 40px;
   width: 350px;
   height: 100px;
 }
 
 .absolute03 {
-  position: absolute;
   color: red;
   background-color: darkseagreen;
   font-size: 200%;
-  top: 430px;
-  left: 900px;
   width: 100px;
 }
 </style>
