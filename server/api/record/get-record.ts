f18d44e3f850c5076bfe8797859cbf39818ead4d
Shion Ichikawa <shion1305@gmail.com>
2023-05-23 12:35:42 +0900

    ✨ Recordページ

--- server/api/record/get-record.ts
+++ server/api/record/get-record.ts
@@ -3,17 +3,32 @@ import admin from '~/server/firebase-admin'
 const db = admin.firestore()
 
 export default defineEventHandler(async (event) => {
-  const results = await db
+  console.log(event.context.userID)
+  const results: Result[] = await db
     .collection('results')
     .where('users', 'array-contains', event.context.userID)
     .get()
     .then((snapshot) => {
+      console.log(snapshot.size)
       const results: any[] = []
       snapshot.forEach((doc) => {
-        results.push(doc.data())
+        results.push(doc.data() as Result)
       })
-      return {
-        records: results
-      }
+      return results
     })
+  // for (const result of results) {
+  //   const blackUser = await db
+  //     .collection('users')
+  //     .where('userID', '==', result.black_user)
+  //     .get()
+  //   const whiteUser = await db
+  //     .collection('users')
+  //     .where('userID', '==', result.white_user)
+  //     .get()
+  //   result.black_user = blackUser.docs[0].data().name
+  //   result.white_user = whiteUser.docs[0].data().name
+  // }
+  return {
+    records: results
+  }
 })
